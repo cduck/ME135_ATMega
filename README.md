f2e10f556d51d2af2eb11e169bb4d8ce8a469c5d
Casey Duckering <cduckering@gmail.com>
2015-04-16 01:17:44 -0700

    Drive 4 servos and 4 motors
    Updates servo and motor positions every time a new command is sent over UART.
    
    Current command format (Hex):
    
    <header> <motor0> <motor1> <motor2> <motor3> <servo0>...<servo3>
    FFFFFFFF 00006400 00006400 00009CFF 00006400 0000 3C00 7800 B400
    
    Motor values and servo positions are little endian.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ME135 Project: Foosball
 Man in the middle to convert UART commands to I2C and PWM outputs.
+
+Current command format (Hex):
+
+    [header] [motor0] [motor1] [motor2] [motor3] [servo0]...[servo3]
+    FFFFFFFF 00006400 00006400 00009CFF 00006400 0000 3C00 7800 B400
+
+Motor values and servo positions are little endian.
+
+UART is 115200 baud.
+
+Grizzly addresses are 0x09, 0x0A, 0x0B, 0x0E respectively.
+
--- button/button.ino
+++ button/button.ino
@@ -7,16 +7,19 @@
 // Choose to send I2C commands to only certain motors by setting its addres to 0.
 // If a motor is not connected but this program tries to send to it, then the
 // program will freeze.
-const byte i2cAddr[NUM_CONTROL] = {0x09, 0x00, 0x00, 0x00};
+const byte i2cAddr[NUM_CONTROL] = {0x09, 0x0A, 0x0B, 0x0E};
 //                                {0x09, 0x0A, 0x0B, 0x0E};
 const int servoPins[NUM_CONTROL] = {4, 5, 6, 7};
+
+// Servo send
 Servo servos[NUM_CONTROL];
 
+// Grizzly send
 #define writeArr(A) (A, sizeof(A))
-#define WireSendArr(ADDR, DATA) Wire.beginTransmission(ADDR);\
+#define WireSendArr(ADDR, DATA) {Wire.beginTransmission(ADDR);\
                                 Wire.write(DATA, sizeof(DATA));\
-                                Wire.endTransmission();
-// First byte is address; the rest is data.
+                                Wire.endTransmission();}
+// First byte is address; the rest is data.  (little endian byte order)
 byte disableB[] = {0x1, 0b00000000,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
 byte startB[] = {0x1, 0b00010011,0x0,0x0,0x0,0x0,100,0x0,0x0};  // Mode (byte 1) and speed (bytes 4-7)
 byte startBSlow[] = {0x1, 0b00010011,0x0,0x0,0x0,0x0,30,0x0,0x0};
@@ -25,12 +28,19 @@ byte reverseB[] = {0x1, 0b00010011,0x0,0x0,0x0,0x0,0x9C,0xFF,0x0};
 byte reverseBSlow[] = {0x1, 0b00010011,0x0,0x0,0x0,0x0,0xE2,0xFF,0x0};
 byte accelLimit[] = {0x90, 50, 0};  // Acceleration limit (pwm change per ms)
 byte currLimit[] = {0x82, 54, 0};  // Current limit, val=limit*(1024/5)*(1/1000)*(66), 54->4 Amps
-byte setToutB[] = {0x80, 0x0, 0x08};  // Timeout (ms)
+byte setToutB[] = {0x80, 0xFA, 0x00};  // Timeout (ms)
+
+// Command recieve
+#define HEADER_LEN 4
+#define PACKET_LEN 24
+byte buffer[HEADER_LEN+PACKET_LEN];
+int bufferFill = 0;
 
 unsigned long time = 0;
+unsigned long lastTime = 0;
 
 void setup() {
-  Serial.begin(230400);
+  Serial.begin(115200);
   Serial.println("Setup");
   // initialize the LED pin as an output:
   pinMode(LED_PIN, OUTPUT);
@@ -66,11 +76,83 @@ void setup() {
 }
 
 void loop() {
-  int a = HIGH;
+  int a = HIGH, b=0;
   time = millis();
-  
-  WireSendArr(i2cAddr[0], startBSlow);
+
+  recieveSerial();
+
+  if(bufferFill >= HEADER_LEN+PACKET_LEN) {
+    setMotors();
+
+    setServos();
+  }
   
   digitalWrite(LED_PIN, a);
-  a = !a;
+  //Serial.println(bufferFill);
+  if(bufferFill >= HEADER_LEN+PACKET_LEN) {
+    a = a==HIGH ? LOW : HIGH;
+  }
+  
+  lastTime = time;
 }
+
+void recieveSerial() {
+  if(bufferFill >= HEADER_LEN+PACKET_LEN) {
+    bufferFill = 0;
+  }
+
+  int incoming;
+  while(bufferFill < HEADER_LEN+PACKET_LEN && (incoming = Serial.read()) >= 0) {
+    //Serial.write(incoming);
+    buffer[bufferFill] = incoming;
+    if(bufferFill < HEADER_LEN) {  // Wait until valid header is recieved
+      if(incoming == 0xFF ||
+         (bufferFill == HEADER_LEN-1 && incoming == 0xFE)) {
+        bufferFill++;
+      } else {
+        bufferFill = 0;
+        Serial.println("Miss");
+      }
+    } else {  // Recieve the packet
+      bufferFill++;
+    }
+    //Serial.println(bufferFill);
+  }
+}
+
+void setMotors() {
+  int speed = (time/100)%200-100;
+  
+  for(int i=0; i<NUM_CONTROL; i++) {
+    startB[4] = buffer[HEADER_LEN+i*4];
+    startB[5] = buffer[HEADER_LEN+i*4+1];
+    startB[6] = buffer[HEADER_LEN+i*4+2];
+    startB[7] = buffer[HEADER_LEN+i*4+3];
+    WireSendArr(i2cAddr[i], startB);
+  }
+
+  // ...
+}
+
+void setServos() {
+  int angle = (time/100)%180;
+  
+  int *angles = (int*)&(buffer[HEADER_LEN+NUM_CONTROL*4]);  // int is 16 bits
+  for(int i=0; i<NUM_CONTROL; i++) {
+    startB[4] = buffer[HEADER_LEN+i*4];
+    startB[5] = buffer[HEADER_LEN+i*4+1];
+    startB[6] = buffer[HEADER_LEN+i*4+2];
+    startB[7] = buffer[HEADER_LEN+i*4+3];
+    servos[i].write(angles[i]);
+  }
+
+  // ...
+}
+
+
+
+
+
+
+
+
